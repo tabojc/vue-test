d2d3a3d6b5eb1fdaf635789cee265a96751b520a
Juan Taborda <tabordajuan@gmail.com>
2024-12-20 19:06:30 -0400

    fix: update state in textfield

--- src/components/fields/TextField.vue
+++ src/components/fields/TextField.vue
@@ -1,16 +1,11 @@
-<template>
-    <input
-        :type="field.type || 'text'"
-        :name="field.id"
-        :disabled="field.disabled ?? null"
-        :value="modelValue"
-        @keyup="updateData"
-    />
-</template>
-
 <script setup>
-import fieldMixin from '../FieldMixin';
-const emit = defineEmits(['update']);
+import { useMainStore } from '../../stores/main.js';
+const mainStore = useMainStore();
+
+//import fieldMixin from '../FieldMixin';
+
+//const emit = defineEmits(['update']);
+
 const props = defineProps({
     field: {
         type: Object,
@@ -21,16 +16,34 @@ const props = defineProps({
         default: '',
     },
 });
-let { handleChange, debounce } = fieldMixin.setup(props, { emit });
+//let { handleChange, debounce } = fieldMixin.setup(props, { emit });
+/*
 const updateData = (event) => {
     debounce(handleChange, 50)(event);
 };
+*/
 
+const onChange = (event) => {
+  const value = event.target.value;
+  
+  console.log(mainStore.setName(value));
+};
 </script>
+<template>
+    <input
+        :type="field.type || 'text'"
+        :name="field.id"
+        :disabled="field.disabled ?? null"
+        :value="modelValue"
+        @input="onChange"
+    />
+</template>
 <style lang="css" scoped>
 input {
     background: black;
     border-width: 1px;
     padding: 10px;
+    color: #fff;
+    border-color: #626262;
 }
 </style>
